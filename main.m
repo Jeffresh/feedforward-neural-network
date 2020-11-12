2bf0adfa58dad44b6a4f98f205bbc8abf7144c4f
jeffresh <manuel.pallanunez@alum.uca.es>
2020-11-12 10:28:37 +0100

    :recycle: update main
    add predict use and tune learning rate and iterations number

--- main.m
+++ main.m
@@ -1,16 +1,20 @@
-load('wine.data')
+clear 
+load('wine_dataset_clean.mat')
 
-X = wine(:,2:end);
-y = wine(:,1);
+X = wine_dataset_clean(:,2:end);
+y = wine_dataset_clean(:,1);
 
-hidden_layers = [5]
-[theta, topology] = createTopology(X, hidden_layers, y)
+hidden_layers = [20]
+[thetas, topology] = createTopology(X, hidden_layers, y);
 
+nn_thetas = unrollThetas(thetas);
+learning_rate = 0.06
 
+for i=1:1000
+  [J, grad, thetas] = nnCostFunction(nn_thetas, topology, X, y, learning_rate);
+  nn_thetas = unrollThetas(thetas);
+end
 
-
-
-
-
-
-
+model = rollThetas(nn_thetas, topology)
+pred = predict(model, X)
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
