fedb2b57f7d9a1991922c2c2efa6baa6f7011c8a
jeffresh <manuel.pallanunez@alum.uca.es>
2020-11-12 10:27:12 +0100

    :sparkles: add descent gradient

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -1,4 +1,4 @@
-function [J, grad] = nnCostfunction(nn_thetas, topology, X, y)
+function [J, grad, thetas] = nnCostfunction(nn_thetas, topology, X, y, learning_rate)
   m = size(X,1);
   
   thetas = rollThetas(nn_thetas, topology);
@@ -19,36 +19,38 @@ function [J, grad] = nnCostfunction(nn_thetas, topology, X, y)
    h = sigmoid(z{length(topology)});
   
   y_vec = encodeY(y);
-  
+  thetas_w = thetas;
   for i=1:length(thetas)
     theta = thetas{i};
     thetas{i} = theta(:, 2:end);
   end
   
-  J = (-sum(y_vec(:) .* log(h(:)) + (1-y_vec(:)).*log(1-h(:)))/m);
+  J = (-sum(y_vec(:) .* log(h(:)) + (1-y_vec(:)).*log(1-h(:)))/m)
  
   % Backpropagation
   
   delta{length(topology)} = (h - y_vec);
-  theta_grad{length(topology)-1} = 1/m.*(delta{length(topology)} * a{length(a)}')
+  theta_grad{length(topology)-1} = 1/m.*(delta{length(topology)} * a{length(a)}');
 
   
   for i = length(topology)-1:-1:2
-    delta{i} = thetas{i}' * delta{i+1} .*sigmoidGradient(z{i})
+    delta{i} = thetas{i}' * delta{i+1} .*sigmoidGradient(z{i});
     if(i~=2)
-      theta_grad{i-1} =1/m.*(delta{i} * a{i-1}')
+      theta_grad{i-1} =1/m.*(delta{i} * a{i-1}');
     end
   end
   
-  theta_grad{1} =1/m.*(delta{2} * a{1})
+  theta_grad{1} =1/m.*(delta{2} * a{1});
   
   
    
   grad = [];
   for i=1:length(theta_grad)
-    td = theta_grad{i};
+    td = theta_grad{i} * learning_rate;
+    thetas_w{i} = thetas_w{i} - td;
     grad = [grad; td(:)];
   end
-   
   
+  thetas = thetas_w;
+     
   
