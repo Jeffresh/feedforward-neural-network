4cf2f9f3c93b38ea6c62a449e170a5422417cc1d
jeffresh <manuel.pallanunez@alum.uca.es>
2020-11-12 10:29:26 +0100

    :art: add semicolons to skip output

--- randInitializeWeights.m
+++ randInitializeWeights.m
@@ -1,5 +1,5 @@
 function W = randInitializeWeights(L_in, L_out, epsilon=0.12)
-  W = zeros(L_out, 1 + L_in)
-  W = rand(L_out, 1 + L_in) * 2 * epsilon - epsilon
-  W(:, 1) = 1
+  W = zeros(L_out, 1 + L_in);
+  W = rand(L_out, 1 + L_in) * 2 * epsilon - epsilon;
+  W(:, 1) = 1;
 endfunction
